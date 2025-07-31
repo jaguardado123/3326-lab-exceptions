c736d86fa53264638b216bdb6690a575c09e8d9a
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-07-31 13:11:00 -0500

    Update ArrayException.java

--- src/ArrayException.java
+++ src/ArrayException.java
@@ -2,7 +2,10 @@ public class ArrayException {
 
     // Use this array for your lab assignment.
     private int[] lab_array = {7, 5, 3, 12, 51, 72, 101};
+    
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
     }
+
+    // Create your valueAt() method here.
 }
