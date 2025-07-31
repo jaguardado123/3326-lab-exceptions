e7f444a7428363d8e2a14879aa1234f89f2c7fa8
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-07-31 13:10:20 -0500

    Update ArrayException.java

--- src/ArrayException.java
+++ src/ArrayException.java
@@ -2,7 +2,7 @@ public class ArrayException {
 
     // Use this array for your lab assignment.
     private int[] lab_array = {7, 5, 3, 12, 51, 72, 101};
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
