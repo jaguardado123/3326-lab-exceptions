c8b1442d89ecacef266fe6f216964329fbbcb60a
Joselito A Guardado <36009999+jaguardado123@users.noreply.github.com>
2025-07-31 13:18:03 -0500

    Update ArrayException.java

--- src/ArrayException.java
+++ src/ArrayException.java
@@ -1,11 +1,18 @@
 public class ArrayException {
 
     // Use this array for your lab assignment.
-    private int[] lab_array = {7, 5, 3, 12, 51, 72, 101};
-    
-    public static void main(String[] args) {
-        
+    private int[] lab_array;
+    private int current = 0;
+
+    public ArrayExceptions(int size) {
+        lab_array = new int[size];
     }
 
+    public void insert(int value) {
+        // Add try and catch here too.
+        lab_array[current] = value;
+        current++;
+    }
+    
     // Create your valueAt() method here.
 }
